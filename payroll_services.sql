2592cd14dbd7f7f529f6c7d6dd24dd2b7550e759
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-23 13:04:02 +0530

    Modified: Create Table

--- payroll_services.sql
+++ payroll_services.sql
@@ -5,3 +5,11 @@ select * from sys.databases;
 use payroll_service;
 
 select db_name(); 
+
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(100) not null,
+salary money not null,
+start date not null
+);
