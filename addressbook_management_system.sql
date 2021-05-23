6ce53ac02c93645d065589ba8b26ccb51305df4b
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-23 13:59:05 +0530

    Added: Insert details into database

--- addressbook_management_system.sql
+++ addressbook_management_system.sql
@@ -18,3 +18,10 @@ Zip varchar(10) not null,
 PhoneNumber varchar(15) not null,
 Email varchar(50) not null
 );
+
+insert into addressbook values
+('Santanu','Mohapatra','Chandrama Complex,Kharabela Nagar','Bhubaneswar','Odisha','751001','6294476694','mohapatra.santanu123@gmail.com'),
+('Sasmita','Dash','Chandrama Complex,Kharabela Nagar','Bhubaneswar','Odisha','751001','9090223112','dash.sasmita123@gmail.com'),
+('Anindita','Dash','Electronic City','Bengaluru','Karnataka','568687','8478567843','aninditadash@gmail.com'),
+('Nihar Ranjan','Mishra','Electronic City','Bengaluru','Karnataka','568687','8790987654','niharranjanmishra@gmail.com'),
+('Anshumaan','Dash','VIP Road','Puri','Odisha','763011','8280946694','anshumaandash@outlook.com')
