2f1c236bd74a933186327ff901fae142fd9a99f7
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-23 13:49:13 +0530

    Added: Added contents to the database

--- addressbook_management_system.sql
+++ addressbook_management_system.sql
@@ -5,3 +5,16 @@ select * from sys.databases;
 use payroll_service;
 
 select db_name(); 
+
+create table addressbook
+(
+id int identity(1,1) primary key,
+Firstname varchar(30) not null,
+Lastname varchar(30) not null,
+Address varchar(50) not null,
+City varchar(30) not null,
+State varchar(30) not null,
+Zip varchar(10) not null,
+PhoneNumber varchar(15) not null,
+Email varchar(50) not null
+);
